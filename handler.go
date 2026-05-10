877402b018b662b67d9f34158c73c2693b83cafd
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2026-05-10 16:38:54 +0200

    feat: add WWW-Authenticate header for SCIM authorization

--- handler.go
+++ handler.go
@@ -99,6 +99,8 @@ func (h *Handler) authMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		auth := c.GetHeader("Authorization")
 		if subtle.ConstantTimeCompare([]byte(auth), []byte("Bearer "+h.token)) != 1 {
+			// https://www.rfc-editor.org/rfc/rfc7235#section-4.1
+			c.Header("WWW-Authenticate", `Bearer realm="scim"`)
 			scimJSON(c, http.StatusUnauthorized, scimError("401", "Unauthorized"))
 			c.Abort()
 			return
